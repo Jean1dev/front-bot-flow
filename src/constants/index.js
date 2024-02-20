41d34d4f53613155ef192bf35d005b94cb6a3197
augusto.savi <augusto.savi@betha.com.br>
2024-02-20 10:16:27 -0300

    feat: :sparkles: adicionando login com keycloak

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,10 @@
+//prod
 export const BaseUrlApiEngine = 'https://bot-builder-engine-2242d70bd3b3.herokuapp.com'
+
+// local
+// export const BaseUrlApiEngine = 'http://localhost:8080'
+export const  KEYCLOAK_CLIENT_CONFIG = {
+    url: `http://localhost:8080`,
+    realm: "bot-flow",
+    clientId: "react-app"
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,22 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async';
 
+import { ReactKeycloakProvider } from "@react-keycloak/web"
+import Keycloak from 'keycloak-js'
+import {KEYCLOAK_CLIENT_CONFIG} from './constants'
+
+const keycloakClient = new Keycloak(KEYCLOAK_CLIENT_CONFIG)
+
+const initOptions = { pkceMethod: 'S256' , onLoad: 'login-required'}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
-  </React.StrictMode>,
+  <ReactKeycloakProvider authClient={keycloakClient} initOptions={initOptions}>
+    <React.StrictMode>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </React.StrictMode>
+  </ReactKeycloakProvider>
 )
