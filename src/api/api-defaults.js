ab95af9fa1e1112ecd18da0086f2a447f1774438
Jeanluca <jeanlucafp@gmail.com>
2024-05-17 15:39:36 -0300

    feat: default error handling

--- src/api/api-defaults.js
+++ src/api/api-defaults.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { BaseUrlApiGerenciamentoDados } from "../constants"
+import { toastError } from "../utils/toasts-utils"
 
 export function isDev() {
     return process.env.NODE_ENV === 'development'
@@ -21,3 +22,34 @@ export const httpApiGerenciamentoDados = axios.create({
 export function addAuthorizationHeader(token) {
     httpApiGerenciamentoDados.defaults.headers.common['Authorization'] = token
 }
+
+httpApiGerenciamentoDados.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    console.log(error.code);
+
+    if (error.code === 'ECONNABORTED') {
+        toastError('Tempo de conexão excedido');
+    }
+
+    if (error?.response?.status === 403) {
+        toastError('Sessão expirada, necessario novo login');
+    }
+
+    if (error?.response?.status === 400) {
+        toastError(error.response?.data?.message);
+    }
+
+    if (error?.response?.status >= 500) {
+        toastError('Erro interno do servidor');
+    }
+
+    const messages = error.response?.data?.details || null;
+    if (messages) {
+        messages.forEach((element) => {
+            toastError(element);
+        });
+    }
+
+    throw error;
+});
